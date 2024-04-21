aefc2fcd1a8741a5fb1e2b15ddab6d27ef617968
Behzod <behzod96447@internet.ru>
2024-04-21 16:30:09 +0500

    ui optimized

--- app/src/main/java/dev/bogibek/nutritionxorazm/ui/activity/UserFieldActivity.kt
+++ app/src/main/java/dev/bogibek/nutritionxorazm/ui/activity/UserFieldActivity.kt
@@ -1,6 +1,8 @@
 package dev.bogibek.nutritionxorazm.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +18,11 @@ class UserFieldActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val intent = Intent(this,MainActivity::class.java)
+        val start : Button = findViewById(R.id.btnSave)
+        start.setOnClickListener {
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/dev/bogibek/nutritionxorazm/ui/fragment/HomeFragment.kt
+++ app/src/main/java/dev/bogibek/nutritionxorazm/ui/fragment/HomeFragment.kt
@@ -5,16 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 import dev.bogibek.nutritionxorazm.R
+import dev.bogibek.nutritionxorazm.databinding.FragmentHomeBinding
 
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+
+    private lateinit var binding:FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews(view)
     }
-
     private fun initViews(view: View) {
-
+        binding.apply {
+            ivBreakfast.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_breakfastFragment)
+            }
+            ivLunch.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_lunchFragment)
+            }
+            ivDinner.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_dinnerFragment)
+            }
+        }
     }
 }
