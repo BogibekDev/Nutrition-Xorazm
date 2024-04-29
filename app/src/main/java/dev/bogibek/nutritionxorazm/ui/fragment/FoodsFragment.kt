4e0f7ec101a15c917dfcd4c51333f049822fbf64
Behzod <behzod96447@internet.ru>
2024-04-30 02:24:44 +0500

    bug in FoodsFragment fixed

--- app/src/main/java/dev/bogibek/nutritionxorazm/ui/fragment/FoodsFragment.kt
+++ app/src/main/java/dev/bogibek/nutritionxorazm/ui/fragment/FoodsFragment.kt
@@ -6,16 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dev.bogibek.nutritionxorazm.databinding.FragmentBreakfastBinding
+import dev.bogibek.nutritionxorazm.databinding.FragmentFoodsBinding
 
 class FoodsFragment : Fragment() {
 
-    private lateinit var binding:FragmentBreakfastBinding
+    private lateinit var binding:FragmentFoodsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentBreakfastBinding.inflate(layoutInflater,container,false)
+        binding = FragmentFoodsBinding.inflate(layoutInflater,container,false)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
