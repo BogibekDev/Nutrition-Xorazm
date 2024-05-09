8df04952401cddf6918bfe5efc6ef4a178b3700f
BogibekDev <BogibekDev@gmail.com>
2024-05-09 22:17:42 +0500

    Commit Information: 1. add card dialog_add_card.xml finish

--- app/src/main/java/dev/bogibek/nutritionxorazm/ui/fragment/PremiumFragment.kt
+++ app/src/main/java/dev/bogibek/nutritionxorazm/ui/fragment/PremiumFragment.kt
@@ -1,5 +1,6 @@
 package dev.bogibek.nutritionxorazm.ui.fragment
 
+import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,18 +8,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
 import dev.bogibek.nutritionxorazm.R
 import dev.bogibek.nutritionxorazm.data.local.SharedPrefs
 
 
 class PremiumFragment : Fragment(R.layout.fragment_premium) {
+    private lateinit var dialog: Dialog
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews(view)
     }
 
     private fun initViews(view: View) {
+        dialog = Dialog(requireContext())
         val plan = SharedPrefs(requireContext()).getString("plan")?:"free"
         val btnFree = view.findViewById<Button>(R.id.btnFree)
         val btnMonth = view.findViewById<Button>(R.id.btnMonth)
@@ -41,15 +46,24 @@ class PremiumFragment : Fragment(R.layout.fragment_premium) {
             }
         }
         btnMonth.setOnClickListener {
-            Toast.makeText(requireContext(), "Not Implemented yet", Toast.LENGTH_SHORT).show()
+            showDialog(60000)
         }
         btnFree.setOnClickListener {
-            Toast.makeText(requireContext(), "Not Implemented yet", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "All ready selected", Toast.LENGTH_SHORT).show()
         }
         btnYear.setOnClickListener {
-            Toast.makeText(requireContext(), "Not Implemented yet", Toast.LENGTH_SHORT).show()
+            showDialog(500000)
         }
 
+    }
+
+    private fun showDialog(money: Int) {
+        dialog.setContentView(R.layout.dialog_add_card)
+        dialog.window?.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        dialog.show()
+//        val bSave = dialog.findViewById<Button>(R.id.btnSaveVolume)
+//        val bCancel = dialog.findViewById<Button>(R.id.btnCancelVolume)
+//        val etVolume = dialog.findViewById<EditText>(R.id.etVolume)
     }
 
 }
