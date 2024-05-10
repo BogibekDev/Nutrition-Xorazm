604953a7a3bf00ab1cd229b16252c9c8063222ab
BogibekDev <BogibekDev@gmail.com>
2024-05-10 10:28:32 +0500

    finished indi o'zgarmidi

--- app/src/main/java/dev/bogibek/nutritionxorazm/ui/fragment/PremiumFragment.kt
+++ app/src/main/java/dev/bogibek/nutritionxorazm/ui/fragment/PremiumFragment.kt
@@ -3,14 +3,15 @@ package dev.bogibek.nutritionxorazm.ui.fragment
 import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import dev.bogibek.nutritionxorazm.R
 import dev.bogibek.nutritionxorazm.data.local.SharedPrefs
 
@@ -24,33 +25,37 @@ class PremiumFragment : Fragment(R.layout.fragment_premium) {
 
     private fun initViews(view: View) {
         dialog = Dialog(requireContext())
-        val plan = SharedPrefs(requireContext()).getString("plan")?:"free"
+        val plan = SharedPrefs(requireContext()).getString("plan") ?: "free"
         val btnFree = view.findViewById<Button>(R.id.btnFree)
         val btnMonth = view.findViewById<Button>(R.id.btnMonth)
         val btnYear = view.findViewById<Button>(R.id.btnYear)
-        when(plan){
+        when (plan) {
             "paid" -> {
                 btnMonth.background.setTint(Color.parseColor("#FF7B01"))
                 btnFree.background.setTint(Color.parseColor("#08A6B0"))
                 btnYear.background.setTint(Color.parseColor("#08A6B0"))
             }
+
             "premium" -> {
                 btnMonth.background.setTint(Color.parseColor("#08A6B0"))
                 btnFree.background.setTint(Color.parseColor("#08A6B0"))
                 btnYear.background.setTint(Color.parseColor("#FF7B01"))
             }
+
             else -> {
                 btnFree.background.setTint(Color.parseColor("#FF7B01"))
                 btnMonth.background.setTint(Color.parseColor("#08A6B0"))
                 btnYear.background.setTint(Color.parseColor("#08A6B0"))
             }
         }
-        btnMonth.setOnClickListener {
-            showDialog(60000)
-        }
         btnFree.setOnClickListener {
             Toast.makeText(requireContext(), "All ready selected", Toast.LENGTH_SHORT).show()
         }
+
+        btnMonth.setOnClickListener {
+            showDialog(60000)
+        }
+
         btnYear.setOnClickListener {
             showDialog(500000)
         }
@@ -59,11 +64,50 @@ class PremiumFragment : Fragment(R.layout.fragment_premium) {
 
     private fun showDialog(money: Int) {
         dialog.setContentView(R.layout.dialog_add_card)
-        dialog.window?.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setLayout(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
         dialog.show()
-//        val bSave = dialog.findViewById<Button>(R.id.btnSaveVolume)
-//        val bCancel = dialog.findViewById<Button>(R.id.btnCancelVolume)
-//        val etVolume = dialog.findViewById<EditText>(R.id.etVolume)
+        val bPay = dialog.findViewById<Button>(R.id.bPay)
+        val ivClose = dialog.findViewById<ImageView>(R.id.ivClose)
+        val tvCardNumber = dialog.findViewById<TextView>(R.id.tvCardNumber)
+        val tvCardDate = dialog.findViewById<TextView>(R.id.tvDate)
+        val tvCardYear = dialog.findViewById<TextView>(R.id.tvYear)
+        val tvCardName = dialog.findViewById<TextView>(R.id.tvCardName)
+        val etCardName = dialog.findViewById<EditText>(R.id.etCardName)
+        val etCardNumber = dialog.findViewById<EditText>(R.id.etCardNumber)
+        val etCardDate = dialog.findViewById<EditText>(R.id.etCardDate)
+        val etCardYear = dialog.findViewById<EditText>(R.id.etCardYear)
+
+        bPay.text = "$money so'm to'lash"
+        ivClose.setOnClickListener{
+            dialog.dismiss()
+        }
+        bPay.setOnClickListener {
+            Toast.makeText(requireContext(), "Payme bilan Integratsiya ishlari ketyapdi", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        etCardName.addTextChangedListener {
+            tvCardName.text = it
+        }
+        etCardDate.addTextChangedListener {
+            tvCardDate.text = it
+        }
+        etCardYear.addTextChangedListener {
+            tvCardYear.text = it
+        }
+
+
+        etCardNumber.addTextChangedListener {
+            if (it?.length == 4 || it?.length == 9 || it?.length == 14) it.append(" ")
+            tvCardNumber.text = it
+        }
+
     }
+
+
+
 
 }
